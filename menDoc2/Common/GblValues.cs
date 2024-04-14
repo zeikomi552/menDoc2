1a6393f104d7e99004989cbdf641faa7a5926782
zeikomi552 <premium.tsubu@gmail.com>
2024-04-15 05:43:20 +0900

    WebView2表示用ファイル作成クラスの作成

--- menDoc2/Common/GblValues.cs
+++ menDoc2/Common/GblValues.cs
@@ -59,5 +59,7 @@ namespace menDoc2.Common
             }
         }
         #endregion
+
+
     }
 }
---
+++ menDoc2/Models/DisplayWebManagerM.cs
@@ -0,0 +1,85 @@
+﻿using Markdig;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace menDoc2.Models
+{
+    /// <summary>
+    /// WebView2で表示するためのHTMLファイル作成クラス
+    /// </summary>
+    public class DisplayWebManagerM
+    {
+        #region ベースとなるHTMLコードの保存場所
+        /// <summary>
+        /// ベースとなるHTMLコードの保存場所
+        /// </summary>
+        public static string BaseHtml { get; set; } = @".\Common\Templete\HtmlCode\BaseHtml.mdtmpl";
+        #endregion
+
+        #region mermaid用JavaScriptのフォルダパス
+        /// <summary>
+        /// mermaid用JavaScriptのフォルダパス
+        /// </summary>
+        private static string JsDirPath { get; set; } = @".\Common\js";
+        #endregion
+
+        #region HTMLコード
+        /// <summary>
+        /// HTMLコード
+        /// </summary>
+        private string BaseHtmlCode { get; set; } = string.Empty;
+        #endregion
+
+        #region HTMLコードの取得処理
+        /// <summary>
+        /// HTMLコードの取得処理
+        /// </summary>
+        /// <param name="markdown">マークダウン文字列</param>
+        /// <returns>HTMLコード</returns>
+        public string GetHtml(string markdown)
+        {
+            try
+            {
+                // テンプレートファイルの読み込み
+                StreamReader html_sr = new StreamReader(BaseHtml, Encoding.UTF8);
+                this.BaseHtmlCode = html_sr.ReadToEnd();
+
+                var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+                var html = Markdig.Markdown.ToHtml(markdown, pipeline);
+                var html_txt = this.BaseHtmlCode.Replace("{menDoc:jsdir}", JsDirPath);
+                return html_txt.Replace("{menDoc:htmlbody}", html);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region HTMLファイルの保存処理
+        /// <summary>
+        /// HTMLファイルの保存処理
+        /// </summary>
+        /// <param name="markdown">マークダウン文字列</param>
+        public void SaveHtml(string markdown)
+        {
+            try
+            {
+                var html = GetHtml(markdown);
+                var fv = FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), fv.CompanyName!, fv.ProductName!, "Temp.html");
+                File.WriteAllText(path, html);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        #endregion
+    }
+}
